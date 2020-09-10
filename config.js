87c25f1f4ac50262f8bb4fdd8f785bed9437703b
Suneel Kumar Khatri <khatrisuneel96@gmail.com>
2020-09-10 14:56:56 +0500

    Firebase config added

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCn8djTrkLM9sMXD7KTjoJwDkB2Tr8mGy0",
+  authDomain: "fir-basics-abfb9.firebaseapp.com",
+  databaseURL: "https://fir-basics-abfb9.firebaseio.com",
+  projectId: "fir-basics-abfb9",
+  storageBucket: "fir-basics-abfb9.appspot.com",
+  messagingSenderId: "618224789562",
+  appId: "1:618224789562:web:a9dde5ddf3e2d71d15ed03",
+};
